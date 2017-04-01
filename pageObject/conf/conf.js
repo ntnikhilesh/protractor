2218ea3b9c76e6e5ca77b7662f3e86c14cb9b531
nikhil636 <ntnikhilesh@gmail.com>
2017-04-01 23:12:01 +0530

    first test using page object done

---
+++ pageObject/conf/conf.js
@@ -0,0 +1,28 @@
+//Configuration file
+
+exports.config={
+  //The address of a running selenium server 
+
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+
+  //Capabilities to be passed to the webdriver instance
+
+  capabilities:
+  {
+    'browserName':'chrome'
+  },
+
+  //Spec patterns are relative to the current working directory when protractor is called
+
+  specs: ['../test/*_spec.js'],
+
+  //Options to be passed to Jasmine-node
+
+  jasmineNodeOpts:
+  {
+    showColors:true,
+    defaultTimeoutInterval:30000
+  }
+  
+
+};
