b1c9895a65edf2efdba202cdd226b5360e79a775
nikhil636 <ntnikhilesh@gmail.com>
2017-04-01 17:24:01 +0530

    using expects

--- zoo_spec.js
+++ zoo_spec.js
@@ -2,20 +2,69 @@
 
 describe ("Adopt an animal on the zoo test site",function(){
 
-  it("should able to adopt an animal", function()
+  it("should able to correct page", function()
   {
     browser.get("http://www.thetestroom.com/jswebapp/"); 
 
-    element(by.model("person.name")).sendKeys("I will not subscribe");
+    //using expect() we can perform validation
+
+    expect(browser.getCurrentUrl()).toContain("jswebapp");
+    expect(browser.getCurrentUrl()).toEqual("http://www.thetestroom.com/jswebapp/");
+
+  
+  });
+
+  it("should able to correct text", function()
+  {
+    browser.get("http://www.thetestroom.com/jswebapp/"); 
+
+    //using expect() we can perform validation
+
+      var textMessage="I will not subscribe"
+    element(by.model("person.name")).sendKeys(textMessage);
     element(by.binding("person.name")).getText().then(function(text)
     {
-      console.log(text);
+      expect(text).toEqual("I will not subscribe");
     });
+  
+  });
+
+  it("should able to correct number of item in dropdown", function()
+  {
+    browser.get("http://www.thetestroom.com/jswebapp/"); 
+
+    //using expect() we can perform validation
+
+    element(by.buttonText("CONTINUE")).click();
+    element(by.model("animal")).$("[value='2']").click();
+
+    element.all(by.css(".ng-pristine option")).then(function(items){
+      expect(items.length).toBe(4);
+      expect(items[1].getText()).toBe("George the Turtle");
+    })
+    
+
     element(by.buttonText("CONTINUE")).click();
 
-    element(by.model("animal")).$('[value="1"]').click();
-    element(by.partialButtonText("CONT")).click();
-  })
+   
+  
+  
+  });
+
+
+  it("should check user is on Thank you page ", function()
+  {
+    browser.get("http://www.thetestroom.com/jswebapp/"); 
+
+    //using expect() we can perform validation
+
+    element(by.buttonText("CONTINUE")).click();
+    element(by.buttonText("CONTINUE")).click();
+    expect(browser.getCurrentUrl()).toContain("confirm");
+   
+  
+  
+  });
 })
 
 
